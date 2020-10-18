3d2c33d91e46a0a5f025afc4dfedd38077d0ff29
imam riyadi <imamriyadi32@gmail.com>
2020-10-18 09:09:33 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
+const {userModel} = require("../model/usersModel");
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/',  (req, res) =>{
+  try{
+    userModel.find({}).then((result)=>{
+      res.status(200).json({
+        status: "success",
+        data:result
+      });
+    }).catch(reason => {
+      res.status(404).json({
+        status: "error",
+        data: reason
+      })
+    });
+  }catch (e) {
+    res.status(404).json({
+      status: "error",
+      message: e.message.toString()
+    })
+  }
 });
+
+router.post("/",function (req,res,next) {
+  try {
+    const data = req.body;
+    const userModels = new userModel(data);
+    userModels.save().then(()=>{
+      res.status(200).json({
+        status: "success",
+        messages: "success add users"
+      });
+    });
+  }catch (e) {
+    res.status(400).json({
+      status: "error",
+      messages: e.toString()
+    });
+  }
+});
+
 
 module.exports = router;
